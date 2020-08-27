2fbb2a46acea78d90c7475ae506c701ca439b959
Raghavendra KULKARNI <RAGHAVENDRA.VK@GMAIL.COM>
2020-08-27 16:42:21 +0530

    Updated IP on 27 Aug

--- src/test/java/TestNG/addbook.java
+++ src/test/java/TestNG/addbook.java
@@ -15,7 +15,7 @@ public class addbook {
 	public void launch() {
 		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
 		  driver = new ChromeDriver();
-		  driver.get("http://18.188.200.132:32768/addressbook/");
+		  driver.get("http://3.21.230.75:32768/addressbook/");
 
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
